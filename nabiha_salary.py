e477bbe675df6352e65e331a2d9a9ce3612db99a
DanaDergham <dndergham@gmail.com>
2025-02-21 19:00:25 +0200

    inserted input prompts for savings,rent and electricity percentages

--- nabiha_salary.py
+++ nabiha_salary.py
@@ -1,2 +1,6 @@
 salary = float(input("Please enter your salary for the month:"))
-month=input("Enter the name of the month")
+month=input("Enter the name of the month:")
+
+savings_percentage= float(input("Please enter the percentage for savings: "))
+rent_percentage= float(input("Please enter the percentage for rent: "))
+electricity_percentage=float(input("Please enter the electricity percentage: "))
